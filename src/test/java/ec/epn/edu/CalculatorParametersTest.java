9595dec409adfe3ba96e8a6b94320bcb55fb8bdc
jake <jake.benavides@epn.edu.ec>
2026-04-25 19:57:20 -0500

    Suportin parameters unit tes.

---
+++ src/test/java/ec/epn/edu/CalculatorParametersTest.java
@@ -0,0 +1,23 @@
+package ec.epn.edu;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorParametersTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "2,  4,  6",
+            "1,  8,  9",
+            "2,  5,  7",
+            "7,  3,  10",
+            "22, 4,  26",
+            "12, 14, 26"
+    })
+    public void given_parameters_when_addition_then_summation(int a, int b, int expected) {
+        Calculator c = new Calculator();
+        int actual = c.addition(a, b);
+        assertEquals(expected, actual);
+    }
+}
